f56b2d6595ed99e91f4f3f3be314a1a5e9f85b08
Brent <bjcameron@gmail.com>
2020-04-13 11:01:44 -0600

    Adding Routing to NavBar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 
 //function to hold the nav bar of the app
@@ -6,13 +7,16 @@ import React from 'react';
 function NavBar () {
 
     return (
-        <div className='bg-green-300'>
-            <ul className='inline-block p-4 font-bold text-xl text-gray-700'>
-                <li className='inline-block px-4'>About</li>
-                <li className='inline-block px-4'>Add A plant</li>
-            </ul>
-        </div>
-
+        <Router>
+            <nav>
+                <div className='bg-green-300'>
+                    <ul className='inline-block p-4 font-bold text-xl text-gray-700'>
+                        <li className='inline-block px-4'>About</li>
+                        <li className='inline-block px-4'>Add A plant</li>
+                    </ul>
+                </div>
+            </nav>
+        </Router>
     );
 }
 
